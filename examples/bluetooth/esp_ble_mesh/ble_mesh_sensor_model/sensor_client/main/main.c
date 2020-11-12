456123febd08e877a12da97404012e2ba3611407
yichin <s880782002@gmail.com>
2020-11-13 00:58:10 +0900

    temporary done

--- examples/bluetooth/esp_ble_mesh/ble_mesh_sensor_model/sensor_client/main/main.c
+++ examples/bluetooth/esp_ble_mesh/ble_mesh_sensor_model/sensor_client/main/main.c
@@ -26,6 +26,11 @@
 #include "ble_mesh_example_init.h"
 #include "board.h"
 
+#include "esp_types.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "driver/periph_ctrl.h"
 #include "driver/timer.h"
 
 #ifndef ESP_BLE_MESH_CID_NVAL
@@ -34,7 +39,25 @@
 
 #define TAG "EXAMPLE"
 
+#define TIMER_DIVIDER         16  //  Hardware timer clock divider
+#define TIMER_SCALE           (TIMER_BASE_CLK / TIMER_DIVIDER)  // convert counter value to seconds
 #define TIMER_INTERVAL0_SEC   (3.4179) // sample test interval for the first timer
+#define TIMER_INTERVAL1_SEC   (5.78)   // sample test interval for the second timer
+#define TEST_WITHOUT_RELOAD   0        // testing will be done without auto reload
+#define TEST_WITH_RELOAD      1        // testing will be done with auto reload
+
+/*
+ * A sample structure to pass events
+ * from the timer interrupt handler to the main program.
+ */
+typedef struct {
+    int type;  // the type of timer's event
+    int timer_group;
+    int timer_idx;
+    uint64_t timer_counter_value;
+} timer_event_t;
+
+xQueueHandle timer_queue;
 
 #define CID_ESP             0x02E5
 
@@ -59,6 +82,23 @@ static uint8_t  dev_uuid[ESP_BLE_MESH_OCTET16_LEN];
 static uint16_t server_address[ESP_BLE_MESH_OCTET16_LEN] = {0xFFFF};
 static uint16_t sensor_prop_id;
 
+/*
+ * Final data for temperature , pressure , humidity
+ */
+
+static union {
+    double  temp_act;
+    uint8_t temp_data[8];
+} my_temp;
+static union {
+    double  press_act;
+    uint8_t press_data[8];
+} my_press;
+static union {
+    double  hum_act;
+    uint8_t hum_data[8];
+} my_hum;
+
 static struct esp_ble_mesh_key {
     uint16_t net_idx;
     uint16_t app_idx;
@@ -77,6 +117,8 @@ static esp_ble_mesh_cfg_srv_t config_server = {
     .net_transmit = ESP_BLE_MESH_TRANSMIT(2, 20),
     .relay_retransmit = ESP_BLE_MESH_TRANSMIT(2, 20),
 };
+
+static void timer_example_evt_task(void *arg);
 
 static esp_ble_mesh_client_t config_client;
 static esp_ble_mesh_client_t sensor_client;
@@ -103,8 +145,6 @@ static esp_ble_mesh_prov_t provision = {
     .prov_start_address = 0x0005,
 };
 
-static void example_ble_mesh_send_sensor_message(uint32_t opcode);
-
 /*
  * @ brief this block is used to deal with the timer and periodic event
  */
@@ -119,13 +159,46 @@ static uint32_t send_opcode[] = {
     [4] = ESP_BLE_MESH_MODEL_OP_SENSOR_SERIES_GET,
 };
 
-static IRAM_ATTR timer_cb(){
-    example_ble_mesh_send_sensor_message(send_opcode[3]);
+static void IRAM_ATTR timer_cb(void *para){
+    timer_spinlock_take(TIMER_GROUP_0);
+    int timer_idx = (int) para;
+
+    /* Retrieve the interrupt status and the counter value
+       from the timer that reported the interrupt */
+    uint32_t timer_intr = timer_group_get_intr_status_in_isr(TIMER_GROUP_0);
+    uint64_t timer_counter_value = timer_group_get_counter_value_in_isr(TIMER_GROUP_0, timer_idx);
+
+    /* Prepare basic event data
+       that will be then sent back to the main program task */
+    timer_event_t evt;
+    evt.timer_group = 0;
+    evt.timer_idx = timer_idx;
+    evt.timer_counter_value = timer_counter_value;
+
+    /* Clear the interrupt
+       and update the alarm time for the timer with without reload */
+    if (timer_intr & TIMER_INTR_T0) {
+        evt.type = TEST_WITHOUT_RELOAD;
+        timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, TIMER_0);
+        timer_counter_value += (uint64_t) (TIMER_INTERVAL0_SEC * TIMER_SCALE);
+        timer_group_set_alarm_value_in_isr(TIMER_GROUP_0, timer_idx, timer_counter_value);
+    } else if (timer_intr & TIMER_INTR_T1) {
+        evt.type = TEST_WITH_RELOAD;
+        timer_group_clr_intr_status_in_isr(TIMER_GROUP_0, TIMER_1);
+    } else {
+        evt.type = -1; // not supported even type
+    }
+
+    /* After the alarm has been triggered
+      we need enable it again, so it is triggered the next time */
+    timer_group_enable_alarm_in_isr(TIMER_GROUP_0, timer_idx);
+
+    /* Now just send the event data back to the main program task */
+    xQueueSendFromISR(timer_queue, &evt, NULL);
+    timer_spinlock_give(TIMER_GROUP_0);
 }
 
-
-
-static void esp_timer_init() {
+static void example_timer_init(int timer_idx, bool auto_reload, double timer_interval_sec) {
     /* Select and initialize basic parameters of the timer */
     timer_config_t config = {
         .divider = TIMER_DIVIDER,
@@ -254,6 +327,7 @@ static void example_ble_mesh_provisioning_cb(esp_ble_mesh_prov_cb_event_t event,
         recv_unprov_adv_pkt(param->provisioner_recv_unprov_adv_pkt.dev_uuid, param->provisioner_recv_unprov_adv_pkt.addr,
                             param->provisioner_recv_unprov_adv_pkt.addr_type, param->provisioner_recv_unprov_adv_pkt.oob_info,
                             param->provisioner_recv_unprov_adv_pkt.adv_type, param->provisioner_recv_unprov_adv_pkt.bearer);
+        xTaskCreate(timer_example_evt_task, "timer_evt_task", 2048, NULL, 5, NULL);
         break;
     case ESP_BLE_MESH_PROVISIONER_PROV_LINK_OPEN_EVT:
         ESP_LOGI(TAG, "ESP_BLE_MESH_PROVISIONER_PROV_LINK_OPEN_EVT, bearer %s",
@@ -634,6 +708,15 @@ static void example_ble_mesh_sensor_client_cb(esp_ble_mesh_sensor_client_cb_even
                         fmt == ESP_BLE_MESH_SENSOR_DATA_FORMAT_A ? "A" : "B", data_len, prop_id);
                     if (data_len != ESP_BLE_MESH_SENSOR_DATA_ZERO_LEN) {
                         ESP_LOG_BUFFER_HEX("Sensor Data", data + mpid_len, data_len + 1);
+                        if (prop_id == 0x0056) {
+                            memcpy(my_temp.temp_data, data + mpid_len, 8);
+                        } else if (prop_id == 0x005b) {
+                            memcpy(my_hum.hum_data, data + mpid_len, 8);
+                        } else if (prop_id == 0x0060) {
+                            memcpy(my_press.press_data, data + mpid_len, 8);
+                        } else {
+                            ;
+                        }
                         length += mpid_len + data_len + 1;
                         data += mpid_len + data_len + 1;
                     } else {
@@ -742,6 +825,26 @@ static esp_err_t ble_mesh_init(void)
     return ESP_OK;
 }
 
+/*
+ * show data
+ */
+static void show_data() {
+    ESP_LOGI(TAG, "temperature: %lf, pressure: %lf, humidity: %lf", my_temp.temp_act, my_press.press_act, my_hum.hum_act);
+}
+
+/*
+ * The main task of this example program
+ */
+static void timer_example_evt_task(void *arg)
+{
+    while (1) {
+        timer_event_t evt;
+        xQueueReceive(timer_queue, &evt, portMAX_DELAY);
+        example_ble_mesh_send_sensor_message(send_opcode[3]);
+        show_data();
+    }
+}
+
 void app_main(void)
 {
     esp_err_t err = ESP_OK;
@@ -757,7 +860,12 @@ void app_main(void)
 
     board_init();
 
+    timer_queue = xQueueCreate(10, sizeof(timer_event_t));
+
     err = bluetooth_init();
+
+    example_timer_init(TIMER_0, TEST_WITHOUT_RELOAD, TIMER_INTERVAL0_SEC);
+
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "esp32_bluetooth_init failed (err %d)", err);
         return;
@@ -770,4 +878,5 @@ void app_main(void)
     if (err != ESP_OK) {
         ESP_LOGE(TAG, "Bluetooth mesh init failed (err %d)", err);
     }
+
 }
